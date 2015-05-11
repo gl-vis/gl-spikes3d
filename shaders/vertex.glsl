e54a331a7631ee415b27e001c269155e3b9894c5
mikolalysenko <mikolalysenko@gmail.com>
2015-05-11 11:53:58 -0400

    fix perspective division bug

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -11,20 +11,15 @@ uniform float lineWidth;
 
 varying vec4 fragColor;
 
-vec3 project(vec3 p) {
-  vec4 clip = projection * view * model * vec4(p, 1.0);
-  return clip.xyz / clip.w;
-}
-
 void main() {
   vec3 vertexPosition = mix(coordinates[0],
     mix(coordinates[2], coordinates[1], 0.5 * (position + 1.0)), abs(position));
 
-  vec3 clipPos = project(vertexPosition);
-  vec3 clipOffset = project(vertexPosition + color);
-  vec2 delta = weight * (clipPos.xy - clipOffset.xy) * screenShape;
+  vec4 clipPos = projection * view * model * vec4(vertexPosition, 1.0);
+  vec2 clipOffset = (projection * view * model * vec4(color, 0.0)).xy;
+  vec2 delta = weight * clipOffset * screenShape;
   vec2 lineOffset = normalize(vec2(delta.y, -delta.x)) / screenShape;
 
-  gl_Position   = vec4(clipPos.xy + 0.5 * lineWidth * lineOffset, clipPos.z, 1.0);
+  gl_Position   = vec4(clipPos.xy + clipPos.w * 0.5 * lineWidth * lineOffset, clipPos.z, clipPos.w);
   fragColor     = color.x * colors[0] + color.y * colors[1] + color.z * colors[2];
 }
