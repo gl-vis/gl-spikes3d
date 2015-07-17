955eb994a4f364da27c52258f69bbff0dded688c
mikolalysenko <mikolalysenko@gmail.com>
2015-07-17 11:57:13 -0500

    intel workaround

--- shaders/index.js
+++ shaders/index.js
@@ -7,5 +7,9 @@ var vertSrc = glslify('./vertex.glsl')
 var fragSrc = glslify('./fragment.glsl')
 
 module.exports = function(gl) {
-  return createShader(gl, vertSrc, fragSrc)
+  return createShader(gl, vertSrc, fragSrc, null, [
+    {name: 'position', type: 'vec3'},
+    {name: 'color', type: 'vec3'},
+    {name: 'weight', type: 'float'}
+  ])
 }
